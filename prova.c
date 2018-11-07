fa67c7cedfc65292670a04d743809e401ce58455
Plutonno <44810944+Plutonno@users.noreply.github.com>
2018-11-07 15:39:58 +0100

    Update prova.c
    prova di commit

--- prova.c
+++ prova.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#define N 10
 int main(){
-  printf("Ciao mondo!\n");
+  int a[N][N];
+  //scan della matrice
+  for(int i=0;i<N;i++)
+    for(int j=0;j<N;j++){
+      printf("elemento (%d,%d):",i+1,j+1);
+      scanf("%d",&a[i][j]);
+    }
+  //stampa della matrice
+  for(int i=0;i<N;i++){
+    printf("\n| ");
+    for(int j=0;j<N;j++){
+      printf("%d ",);
+    }
+    printf("|");
+  }
+  return 0;
 }
